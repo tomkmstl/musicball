0becaf39300dcdfbd1188f3a9381df869e46c166
tomkmstl <tomkmstl@gmail.com>
2026-04-30 20:55:25 -0500

    fixed logout

--- logout.php
+++ logout.php
@@ -1,10 +1,25 @@
 <?php
 // logout.php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+require_once __DIR__ . '/session_boot.php';
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        [
+            'expires' => time() - 42000,
+            'path' => $params['path'] ?? '/',
+            'domain' => $params['domain'] ?? '',
+            'secure' => (bool)($params['secure'] ?? false),
+            'httponly' => (bool)($params['httponly'] ?? true),
+            'samesite' => $params['samesite'] ?? 'Lax',
+        ]
+    );
 }
 
-session_unset();
 session_destroy();
 
 header('Location: index.php');
